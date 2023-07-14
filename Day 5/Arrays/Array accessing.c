3065814d422625c325a40b34c14f77990d714e65
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2023-07-14 12:24:53 +0530

    Create Array accessing.c

---
+++ Day 5/Arrays/Array accessing.c
@@ -0,0 +1,20 @@
+//Array accessing
+
+#include<stdio.h>  
+int main(){      
+int i=0;    
+int marks[5]={20,30,40,50,60};//declaration and initialization of array    
+ //traversal of array    
+for(i=0;i<5;i++){      
+printf("%d \n",marks[i]);    
+}    
+printf("enter index to be accessed :");
+int key;
+scanf("%d",&key);
+
+for(i=0;i<5;i++){      
+    if(marks[i]==key)
+        printf("%d \n",marks[key]);    
+}
+return 0;  
+}    
