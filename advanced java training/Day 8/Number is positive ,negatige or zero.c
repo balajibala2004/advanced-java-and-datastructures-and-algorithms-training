02ea3a3e4731055119012c8cf04b5f111a281259
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2023-07-19 20:27:00 -0700

    Create Number is positive ,negatige or zero.c

---
+++ advanced java training/Day 8/Number is positive ,negatige or zero.c
@@ -0,0 +1,24 @@
+//Number is positive ,negatige or zero.c
+
+import java.util.*;
+
+public class MyClass {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int n= sc.nextInt();
+      if(n>0)
+      {
+          System.out.println(" number is positive");
+      }
+      else if(n<0)
+      {
+          System.out.println("number is negative");
+      }
+      else
+      {
+          System.out.println("number is negative");
+      }
+
+      
+    }
+}
