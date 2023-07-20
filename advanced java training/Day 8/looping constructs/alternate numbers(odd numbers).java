055968a6084e5099a92ab4e029bdb27f95d78c25
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2023-07-19 21:46:52 -0700

    Create alternate numbers(odd numbers).java

---
+++ advanced java training/Day 8/looping constructs/alternate numbers(odd numbers).java
@@ -0,0 +1,12 @@
+Write a program to print alternate numbers starting from 1 to 99 i.e. 1 3 5 7 9 11 13 . . . 95 97 99
+
+import java.util.*;
+ public class alternumber {
+     public static void main(String[] args){
+	for (int i = 1; i < 100; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+    }
+}
