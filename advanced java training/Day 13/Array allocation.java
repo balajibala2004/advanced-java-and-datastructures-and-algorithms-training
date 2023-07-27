0ba440098e0d8798a288ed854ca0e93ade9847ba
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2023-07-27 07:19:20 +0530

    Update Array allocation.java

--- advanced java training/Day 13/Array allocation.java
+++ advanced java training/Day 13/Array allocation.java
@@ -3,15 +3,15 @@ import java.util.Arrays;
 public class myclass {  
         public static void main(String[] args) {  
             String[] sa = new String[7]; // Creating a new Array of Size 7  
-            sa[0] = "A"; // Adding Array elements  
-            sa[1] = "B";  
-            sa[2] = "C";  
-            sa[3] = "D";  
-            sa[4] = "E";  
+            sa[0] = "B"; // Adding Array elements  
+            sa[1] = "A";  
+            sa[2] = "L";  
+            sa[3] = "A";  
+            sa[4] = "J";  
             System.out.println("Original Array Elements:" + Arrays.toString(sa));  
             int numberOfItems = 5;  
-            String newItem = "F"; // Expanding Array Elements Later  
-            String newItem2 ="G";  
+            String newItem = "I"; // Expanding Array Elements Later  
+            String newItem2 ="v";  
             sa[numberOfItems++] = newItem;  
             sa[numberOfItems++] = newItem2;  
             System.out.println("Array after adding two elements:" +    
