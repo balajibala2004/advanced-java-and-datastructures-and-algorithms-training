6ae9e5a226fbc0cc2a240493f9ee6fe72374f936
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-02-18 22:42:29 +0530

    Create pattern_14.java

---
+++ coding ninjas/pattern/pattern_14.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public static void nLetterTriangle(int n) {
+        // Write your code here
+    
+        // Write your code here
+        
+        for(int i=0;i<n;i++)
+        {
+         for(char ch ='A';ch<='A'+i;ch++)
+        {
+            System.out.print(ch + " ");
+            
+        }   
+        System.out.print("\n");
+        }
+    
+    }
+}
