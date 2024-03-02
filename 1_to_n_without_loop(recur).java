27cb545682a4e9467fb0d005a430fedcdf271647
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-03-03 05:11:56 +0530

    Create 1_to_n_without_loop(recur).java

---
+++ 1_to_n_without_loop(recur).java
@@ -0,0 +1,22 @@
+public class Solution {
+    public static int[] printNos(int x) {
+    
+     int[]ans =new int[x];
+
+    recur(x, ans);
+
+
+    return ans;
+    }
+
+	public static void recur(int x , int[] ans)
+    {
+        if(x==0)
+		{
+			return;
+		}
+		recur(x-1, ans);
+		
+		ans[x-1] = x;
+    }
+}
