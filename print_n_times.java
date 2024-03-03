dd1fe9d7817f2e55bdcf7c229dbdc62af2d1072e
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-03-03 10:43:13 +0530

    Create print_n_times.java

---
+++ print_n_times.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Solution {
+    public static List<String> printNtimes(int n){
+        // Write your code here.
+        List<String> result = new ArrayList<>();
+		recur(1,n, result);
+        return result;
+    }
+
+    public static void recur(int x,int y , List<String> result)
+	{
+		if(x>y)
+		{
+			return;
+		}
+		result.add("Coding Ninjas");
+		recur(x+1,y , result);
+	}
+
+    
+}
