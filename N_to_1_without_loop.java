3a3693a1e9807c53527fa974bf271880acad1637
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-03-03 20:47:19 +0530

    Create N_to_1_without_loop.java

---
+++ N_to_1_without_loop.java
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public static int[] printNos(int x) {
+        int[] res = new int[x];
+		int cnt=0;
+		recur(1,x,res,cnt);
+        return  res;
+        }
+        public static void recur(int x,int y,int[] res,int cnt)
+	{
+		
+		if(x>y)
+		{
+			return;
+		}
+		
+		res[cnt++] = y;
+		
+		recur(x,y-1,res,cnt);
+		
+		
+	}
+
+}
