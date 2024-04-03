ec06ad963030cb182170022c2f2b78d0293e703f
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-04-04 00:05:13 +0530

    Create  Count Frequency in a range.java

---
+++ Count Frequency in a range.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+public class Solution {
+
+       public static void main(String[] args) {
+	
+	int[] arr = {1,3,4,3,4,1};
+
+	int[] res = countFrequency(6,4,arr);
+	
+	//System.out.println(Arrays.toString(res));
+	
+	}	
+	
+
+     
+	 public static int[] countFrequency(int n, int x, int []nums){
+	        // Write your code here.
+		 int[] arr = new int[x+1];
+		 
+		 for(int i:nums)
+		 {
+			 arr[i]++;
+		 }
+		 return Arrays.copyOfRange(arr, 1, n+1);
+	    }
+    }
+import java.util.Arrays;
+public class Solution {
+
+       public static void main(String[] args) {
+	
+	int[] arr = {1,3,4,3,4,1};
+
+	int[] res = countFrequency(6,4,arr);
+	
+	//System.out.println(Arrays.toString(res));
+	
+	}	
+	
+
+     
+	 public static int[] countFrequency(int n, int x, int []nums){
+	        // Write your code here.
+		 int[] arr = new int[x+1];
+		 
+		 for(int i:nums)
+		 {
+			 arr[i]++;
+		 }
+		 return Arrays.copyOfRange(arr, 1, n+1);
+	    }
+    }
