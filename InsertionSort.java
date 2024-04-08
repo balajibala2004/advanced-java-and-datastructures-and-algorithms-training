4d531172ca6cb37a7c8883bb179f5a6b3c393774
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-04-08 21:08:51 +0530

    Create InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public static void insertionSort(int[] arr, int size) {
+        //Your code goes here
+        int n = size;
+        for(int i =0;i<n;i++)
+        {
+            int j=i;
+            while(j > 0 && arr[j-1]>arr[j])
+            {
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+}
