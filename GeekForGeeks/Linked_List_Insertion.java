6524cbd0cae50ac4de809cd4b852974feae59068
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-07-01 12:07:10 +0530

    Create Linked_List_Insertion.java

---
+++ GeekForGeeks/Linked_List_Insertion.java
@@ -0,0 +1,41 @@
+class Solution
+{
+    //Function to insert a node at the beginning of the linked list.
+    Node insertAtBeginning(Node head, int x)
+    {
+        // code here
+        
+        
+        
+        Node newn = new Node(x);
+       // newn.data = x;
+        newn.next = head;
+        head = newn;
+        
+        return head;
+        
+    }
+    
+    //Function to insert a node at the end of the linked list.
+    Node insertAtEnd(Node head, int x)
+    {
+        Node newn1 = new Node(x); 
+        if(head == null)
+        {
+            head = newn1;
+            return head;
+        }
+        // code here
+        Node temp = head;
+        while(temp.next!=null)
+        {
+            temp = temp.next;
+        }
+        
+        
+        temp.next = newn1;
+        
+        return head;
+        
+    }
+}
