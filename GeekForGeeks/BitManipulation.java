bf33f47f9e93aeb074b9ad436ff8548824270cee
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-07-04 17:39:01 +0530

    Create BitManipulation.java

---
+++ GeekForGeeks/BitManipulation.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+class Solution {
+    static void bitManipulation(int num, int i) {
+        // code here
+        
+        int t = (num>>i-1)&1;
+        
+        System.out.print(t + " ");
+        
+        int t1 = (1<<i-1)|num;
+        System.out.print(t1 + " ");
+        
+        int t3 = (~(1<<i-1))& num;
+        System.out.print(t3 + " ");
+        
+        
+    }
+}
