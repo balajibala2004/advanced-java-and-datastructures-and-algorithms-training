65c23a2e7a55c3b4c041b7831cc4a3eb258db1fb
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-07-07 17:29:29 +0530

    Create making unique digits.java

---
+++ GeekForGeeks/making unique digits.java
@@ -0,0 +1,29 @@
+class Solution
+{
+    public int[] common_digits(int[] nums)
+    {
+        // code here
+        Map<Integer,Integer> mp = new TreeMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            int num = nums[i];
+            
+            while(num!=0)
+            {
+                int t = num%10;
+                mp.put(t ,mp.getOrDefault(t,0)+1);
+                
+                num/=10;
+            }
+        }
+        int[] arr = new int[mp.size()];
+        int i = 0;
+        for(Map.Entry<Integer,Integer> it : mp.entrySet())
+        {
+            int t = it.getKey();
+            arr[i++] = t;
+        }
+        
+        return arr;
+    }
+}
