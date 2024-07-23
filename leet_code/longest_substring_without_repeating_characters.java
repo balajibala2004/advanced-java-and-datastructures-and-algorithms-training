e27b7ed2b6c1d6c79687b9fae84a53e287ccedff
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-07-23 22:47:11 +0530

    Create longest_substring_without_repeating_characters.java

---
+++ leet_code/longest_substring_without_repeating_characters.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+    
+    public int lengthOfLongestSubstring(String s) {
+
+    
+
+    HashMap<Character,Integer> mpp = new HashMap<>();
+
+
+    int left = 0, right = 0;
+        int n = s.length();
+        int len = 0;
+        while (right < n) {
+           if (mpp.containsKey(s.charAt(right))) left = Math.max(mpp.get(s.charAt(right)) + 1, left);
+           // if (mpp.containsKey(s.charAt(right))) left =(mpp.get(s.charAt(right)) + 1) ;
+            mpp.put(s.charAt(right), right);
+
+            len = Math.max(len, right - left + 1);
+            right++;
+        }
+        return len;
+
+    }
+}
