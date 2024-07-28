1c126e7ee52e45693946fe2f03b8c3cebff4c327
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-07-28 12:43:39 +0530

    Create Binary_Subarray_with_sum.java

---
+++ leet_code/Binary_Subarray_with_sum.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution 
+{
+
+    public static int func(int[] nums, int goal)
+    {
+         int l=0,r=0,sum=0,count=0;
+        if(goal<0)
+        {
+            return 0;
+        }
+        while(r<nums.length)
+        {
+            sum +=nums[r];
+
+           
+            while(sum>goal)
+            {
+                sum-= nums[l];
+                l++;
+            }
+            
+           count = count + (r-l+1);
+           r++;
+        }
+        return count;
+    }
+    public int numSubarraysWithSum(int[] nums, int goal) 
+    {
+
+       int g = func(nums,goal);
+       int g1 = func(nums,goal-1);
+        return (g-g1);
+    }
+}
