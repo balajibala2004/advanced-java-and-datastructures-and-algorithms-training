fa13f8e74898f827ddfcf792511d1f50ff61601a
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-09-20 22:14:23 +0530

    Update Count Frequency in a range.java

--- Count Frequency in a range.java
+++ Count Frequency in a range.java
@@ -12,7 +12,7 @@ public static void main(String[] args){
 	
 
      
-	 public static int[] countFrequency(int n, int x, int []nums){
+	public static int[] countFrequency(int n, int x, int []nums){
 	        // Write your code here.
 		 int[] arr = new int[x+1];
 		 
