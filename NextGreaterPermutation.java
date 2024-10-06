735cd2e1a92804d4c8b62e4d10e9026e0768b346
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-10-06 21:05:36 +0530

    Update NextGreaterPermutation.java

--- NextGreaterPermutation.java
+++ NextGreaterPermutation.java
@@ -19,7 +19,7 @@ public class Solution {
             // reverse the whole array:
             Collections.reverse(A);
             return A;
-    }
+ }
 
         // Step 2: Find the next greater element
         //         and swap it with arr[ind]:
