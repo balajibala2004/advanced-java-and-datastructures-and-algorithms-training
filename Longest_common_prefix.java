d92d561a60e290aedf314e167a9281e27ff14ebe
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2024-10-23 22:16:45 +0530

    Create Longest_common_prefix.java

---
+++ Longest_common_prefix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+
+        Arrays.sort(strs);
+        StringBuilder str = new StringBuilder();
+
+        String first = strs[0];
+        String last = strs[strs.length-1];
+        for(int i=0;i<Math.min(first.length() , last.length());i++)
+        {
+            if(first.charAt(i)!=last.charAt(i))
+            {
+                return str.toString();
+            }
+            str.append(first.charAt(i));
+        }
+
+        return str.toString();
+    }
+}
