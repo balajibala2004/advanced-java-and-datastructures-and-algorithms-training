bfb7e12312f81cf4383f90946e67c8f82c1547a6
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2025-11-27 05:23:57 +0530

    Implement min-heap check function in java
    Added a function to check if an array represents a min-heap.

--- advanced java training/minheap_check.java
+++ advanced java training/minheap_check.java
@@ -1,1 +1,37 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 
+class Main {
+    
+    
+    public static boolean func(int[] arr)
+    {
+        int  n = arr.length;
+        
+        //to check binary tree there is no need to check leaf nodes they doesnot have children
+        for(int i=0;i<n/2-1;i++)
+        {
+            int lc = 2*i+1;
+            int rc = 2*i+2;
+            
+            if(( lc < n && arr[i]>arr[lc]) || ( rc < n && arr[i]>arr[rc]))
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+    
+    
+    
+    public static void main(String[] args) {
+
+
+    int[] arr = {10, 20, 30, 21, 23};
+    int[] arr2 = {0, 20, 30, 25, 15};
+    boolean result = func(arr);
+    
+    System.out.println(result);
+    }
+}
